dd9aa57c898e40df6af5d57ffd77c8a0012a0ff4
Matthias Hess <matthias.hess@gmail.com>
2014-04-11 11:36:55 -0500

    Changes made by Matthias Hess (matthias.hess@gmail.com).

--- sprep1/js/controllers/StackListController.js
+++ sprep1/js/controllers/StackListController.js
@@ -4,15 +4,28 @@ spacedApp.controller('StackListController',
     function StackListController($scope, $log, stackDataService) {
 
         $scope.stackList = {
-            items: [{name:"Test1"},{name:"Test2"},{name:"Test3"}]
+            items: [{name:"Test1", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test2", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test4", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test5", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"},
+                    {name:"Test3", desc:"sdfg sdfg sdfg sd gsdf gsdf g"}]
         };
 
 
 
 
-        stackDataService.getStackList().then(function(data) {
-              $scope.stackList.items = data;
-
-          });
+        
     }
 );
